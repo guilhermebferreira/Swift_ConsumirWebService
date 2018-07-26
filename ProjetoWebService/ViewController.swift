a4dd72e3b9dd103e84f589563b84a9f72a7f5043
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-26 14:10:57 -0300

    Initial Commit

---
+++ ProjetoWebService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetoWebService
+//
+//  Created by labmacmini12 on 26/07/2018.
+//  Copyright © 2018 cursoIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
