ade81c56531812bd9f7708acaae72fb0abd476ec
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-26 15:44:37 -0300

    criando os metodos inicializadores do StarView

--- ProjetoWebService/Star/StarView.swift
+++ ProjetoWebService/Star/StarView.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class StarView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+@IBDesignable class StarView: UIView {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
-    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let bundle = Bundle(for: type(of:self))
+        
+        let view = bundle.loadNibNamed("StarView", owner: nil, options: nil)![0] as! StarView //como ele retorna um array de objetos, eu quero pegar apenas o retorno na posição ZERO
+        
+        view.frame = self.bounds
+        
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.addSubview(view)
+    }
 
 }
