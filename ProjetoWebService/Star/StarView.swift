1603020823b62c894602e041d4e45cc858d89301
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-26 15:29:05 -0300

    criando um controller para gerenciar a RatingBar/StarView

---
+++ ProjetoWebService/Star/StarView.swift
@@ -0,0 +1,21 @@
+//
+//  StarView.swift
+//  ProjetoWebService
+//
+//  Created by labmacmini12 on 26/07/2018.
+//  Copyright © 2018 cursoIOS. All rights reserved.
+//
+
+import UIKit
+
+class StarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
